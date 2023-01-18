aa561d8a8dac1746a7f2ae0fd58746bfbfc8629e
MatiasCordich <m.scordich@gmail.com>
2023-01-17 22:24:22 -0300

    CRUD actiones created

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -4,19 +4,88 @@ import {
   Delete,
   Get,
   HttpStatus,
+  Param,
+  NotFoundException,
   Post,
   Put,
   Res,
+  Query,
 } from "@nestjs/common";
 
 import { CreateProductDto } from "./dto/product.dto";
 
+import { ProductService } from "./product.service";
+
 @Controller("product")
 export class ProductController {
+
+  constructor(private productService: ProductService) {}
+
+  // Crear un producto
+
   @Post("/create")
-  createPost(@Res() res, @Body() createProductDTO: CreateProductDto) {
+  async createPost(@Res() res, @Body() createProductDTO: CreateProductDto) {
+    const product = await this.productService.createProduct(createProductDTO);
     return res.status(HttpStatus.OK).json({
-      msg: "Aca van a ir los productos",
+      msg: "PRODUCTO CREADO",
+      data: product,
     });
   }
+
+  // Obtener lista de productos
+
+  @Get('/')
+  async getProducts (@Res() res) {
+    const products = await this.productService.getProducts()
+    return res.status(HttpStatus.OK).json({
+      data: products,
+    });
+  }
+
+  // Obtener un producto por ID
+
+  @Get('/:id')
+  async getProduct (@Res() res, @Param('id') productID) {
+
+    const product = await this.productService.getProduct(productID)
+
+    if(!product) throw new NotFoundException('Producto inexistente')
+
+    return res.status(HttpStatus.OK).json({
+      data: product,
+    });
+  }
+
+  // Editar producto 
+
+  @Put('/update')
+  async updateProduct (@Res() res, @Body() createProductDto: CreateProductDto, @Query('productID') productID) {
+    
+    const productEdited = await this.productService.updateProduct(productID, createProductDto)
+
+    if(!productEdited) throw new NotFoundException('Producto inexistente')
+
+    return res.status(HttpStatus.OK).json({
+      msg: "PRODUCTO EDITADO",
+      data: productEdited,
+    });
+
+  }
+
+  // Eliminar Producto
+
+  @Delete('/delete')
+  async deleteProduct (@Res() res, @Query('productID') productID) {
+    
+    const productDeleted = await this.productService.deleteProduct(productID)
+
+    if(!productDeleted) throw new NotFoundException('Producto inexistente')
+
+    return res.status(HttpStatus.OK).json({
+      message: "PRODUCTO ELIMINAOD",
+      data: productDeleted
+    });
+  }
+
+
 }
