eb730c634af7f7183b14ceca8065b6cba6d8dd16
MatiasCordich <m.scordich@gmail.com>
2023-01-26 21:46:48 -0300

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { NextFunction, Request, Response } from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Accept");
+    next();
+  });
+
   app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'https://product-list-angular-eight.vercel.app',
-    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+    allowedHeaders: "*",
+    origin: "https://product-list-angular-eight.vercel.app",
+    methods: ["POST", "PUT", "DELETE", "GET"],
     credentials: true,
   });
 
