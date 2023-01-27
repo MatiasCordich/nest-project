202d8abb47711e387c1de400b879afdf8ee1cd55
MatiasCordich <m.scordich@gmail.com>
2023-01-26 21:36:28 -0300

    cors modified

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors()
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'https://product-list-angular-eight.vercel.app',
+    credentials: true,
+  });
+
   await app.listen(5000);
 }
 bootstrap();
